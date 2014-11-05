84b8914e5f77de1df51c83d0580c962e199fe2ca
Alexander Henka <Alexander.Henka@googlemail.com>
2014-11-05 11:11:19 +0100

    initial commit

---
+++ core/index.js
@@ -0,0 +1,26 @@
+/**
+ *  index.js is part of PinotGris
+ *  Copyright (c) by Alexander Henka, 05.11.14.
+ *  Project URL: https://github.com/OnkelHenky/Merlot
+ *
+ * +--------------------------------------------------------------------------+
+ * | LICENSE INFORMATION                                                      |
+ * | ===================                                                      |
+ * |                                                                          |
+ * | Licensed under the Apache License, Version 2.0 (the "License");          |
+ * | you may not use this file except in compliance with the License.         |
+ * | You may obtain a copy of the License at                                  |
+ * |                                                                          |
+ * | http://www.apache.org/licenses/LICENSE-2.0                               |
+ * |                                                                          |
+ * | Unless required by applicable law or agreed to in writing, software      |
+ * | distributed under the License is distributed on an "AS IS" BASIS,        |
+ * | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
+ * | See the License for the specific language governing permissions and      |
+ * | limitations under the License.                                           |
+ * |                                                                          |
+ * | For more information see:                                               |
+ * | https://github.com/OnkelHenky/PinotGris/blob/master/README.md           |
+ * |                                                                          |
+ * +--------------------------------------------------------------------------+
+ */
